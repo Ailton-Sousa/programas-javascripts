57ec52ad0c679a9a5de9fcd6bf86abf6e3d89575
Ailton Sousa <ailton2017oliveira@gmail.com>
2023-08-14 16:21:37 -0300

    Estudo javaScript

--- programa05.js
+++ programa05.js
@@ -1,2 +1,7 @@
-document.getElementById("view01").innerHTML = window.document.URL;
-document.getElementById("view02").innerHTML = Date();
+// Lista de mrtodos para pegar elementos da DOM
+
+document.getElementById("view01").innerHTML = "modificado por id";
+document.getElementsByClassName(".view02").innerHTML = "Ola";
+document.getElementsByName("nome-nome"). innerHTML = "mode-nome";
+document.getElementsByTagName("span");
+document.querySelector("body").style.background = "blue";
