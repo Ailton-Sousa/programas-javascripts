71760cf9bd18290ea4aa43514c54b72c35cbad84
Ailton Sousa <ailton2017oliveira@gmail.com>
2023-08-21 23:25:08 -0300

    Estudo javaScript
    Manipulação DOM

---
+++ programa-dom.js
@@ -0,0 +1,15 @@
+
+//Seleção por ID
+
+
+
+let model01 = document.getElementById("id-h1");
+    model01.style.color="red";
+    model01.style.background = "orage";
+    model01.style.fontFamily = "arial";
+    model01.style.borderRadius = "30px";
+
+    //Seleção por Classe
+    let model02 = document.getElementsByClassName("class-sele");
+    
+    model02[0].innerHTML = model02.length;
